890ebfe5fab848845bcb165e0c6c1311c6e39fd7
Saman M. Mohaghegh <63525638+samandeveloper@users.noreply.github.com>
2023-01-15 00:04:09 -0700

    Update image.js

--- controllers/image.js
+++ controllers/image.js
@@ -1,29 +1,13 @@
 //image security
 const Clarifai = require('clarifai');
 const app = new Clarifai.App({
-apiKey: '5811f725850a45738d35b52dfbd00b7f'
-// apiKey: process.env.API_CLARIFAI
+// apiKey: '5811f725850a45738d35b52dfbd00b7f'
+apiKey: process.env.API_KEY
 });
 
 const handleApiCall = (req,res)=>{
-// 	   app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
 	
-// 	app.models.predict('a403429f2ddf4b49b307e318f00e528b', req.body.input)
-	
-// 	app.models.predict({
-// 	model_id: "a403429f2ddf4b49b307e318f00e528b",
-// //      version: "34ce21a40cc24b6b96ffee54aabff139",
-// 	version: "45fb9a671625463fa646c3523a3087d5"
-// 	},
-// 	req.body.input)	
-	
-// 	app.models.predict( {
-//   model_id: "a403429f2ddf4b49b307e318f00e528b",
-//   version: "c0c0ac362b03416da06ab3fa36fb58e3",
-//      }, req.body.input  )
-	
-	app.models
-      .predict(
+app.models.predict(
         {
           id: 'face-detection',
           name: 'face-detection',
@@ -31,13 +15,13 @@ const handleApiCall = (req,res)=>{
           type: 'visual-detector',
         }, req.body.input)
 	
-	
       .then(data=>{
       	res.json(data);
       })
       .catch(err=>res.status(400).json('unable to work with API'))
     }
-//image 
+
+        //image 
 	const handleImage = (req,res,db)=>{
 	const {id} = req.body;
 	db('users')
