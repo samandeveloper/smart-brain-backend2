efdb9b7f07324f628425482042797b12f32f384c
Saman M. Mohaghegh <63525638+samandeveloper@users.noreply.github.com>
2023-01-15 00:10:03 -0700

    Update server.js

--- server.js
+++ server.js
@@ -13,47 +13,24 @@ const image = require('./controllers/image');
 const app = express();
 const path = require('path');
 
-// process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0; 
 const db = knex({
   client: 'pg',
   connection: {
 	
 connectionString: 'postgres://smart_brain_data_user:iHR8jLUrAaqkiN6cn6fh5CLmdPwFfGMb@dpg-cf0ct3kgqg45vesh0480-a/smart_brain_data',
-// Hostname: 'dpg-cf0ct3kgqg45vesh0480-a',
-// Port: 5432,
-// Database: 'smart_brain_data',
-// Username: 'smart_brain_data_user',
-// Password: 'iHR8jLUrAaqkiN6cn6fh5CLmdPwFfGMb',
-// ssl:true
-	  
-	  
-	  hostname: 'dpg-cf0ct3kgqg45vesh0480-a',
-	  username:'smart_brain_data_user',
-	  password: 'iHR8jLUrAaqkiN6cn6fh5CLmdPwFfGMb',
-	  database: 'smart_brain_data',
-	  port: 5432
+
+// 	  hostname: 'dpg-cf0ct3kgqg45vesh0480-a',
+// 	  username:'smart_brain_data_user',
+// 	  password: 'iHR8jLUrAaqkiN6cn6fh5CLmdPwFfGMb',
+// 	  database: 'smart_brain_data',
+// 	  port: 5432
   }
 });
 
-//solution for front
-// app.use(express.static(path.join(__dirname, 'public')));
-// app.get('/', function(req, res) {
-//   res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// });
-
-// app.get('/*', function(req, res) {
-//     res.sendFile('path to index.html');
-// })
 
 app.use(bodyParser.json());
 app.use(cors());
 
-
-//  app.use(express.static(path.join(__dirname, 'public')));
-
-// app.get('*', function(req, res) {
-//   res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// });
 
 //test Endpoint
 app.get('/', (req,res)=>{
