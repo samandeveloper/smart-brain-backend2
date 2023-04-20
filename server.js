e7376ca083dc4256881a35fcb39e7d5c70ebf05f
Saman M. Mohaghegh <63525638+samandeveloper@users.noreply.github.com>
2023-04-20 08:26:29 -0600

    Update server.js

--- server.js
+++ server.js
@@ -16,10 +16,11 @@ const path = require('path');
 const db = knex({
   client: 'pg',
   connection: {
-	host: process.env.DATA_URL,
-	user: process.env.DATA_USER,
-	password: process.env.DATA_PASSWORD,
-	database: process.env.DATA_DATABASE
+// 	host: process.env.DATA_URL,
+// 	user: process.env.DATA_USER,
+// 	password: process.env.DATA_PASSWORD,
+// 	database: process.env.DATA_DATABASE
+	connectionString: process.env.DATA_URL,
   }
 });
 
